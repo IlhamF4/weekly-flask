5b199d319c0f95c4ea0da0ed4019f95f527541ed
IlhamF4 <52096634+IlhamF4@users.noreply.github.com>
2026-04-19 20:07:24 +0700

    Add POST /tasks endpoint and routr handler for /tasks

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,23 @@ def hello():
 	return jsonify({"message": "hello world"})
 	
 tasks = []
-@app.route("/tasks")
+
+
+#return json of tasks
 def get_tasks():
 	return jsonify(tasks)
+
+#adding input from user to tasks
+def add_task(title):
+	tasks.append(title)
+	return jsonify({"message": "added", "task": title})
+
+@app.route("/tasks",methods=["GET","POST"])
+def tasks_route():
+	if request.method == "GET":
+		return get_tasks()
+	elif request.method == "POST":
+		title = request.args.get("title")
+		if not title:
+			return {"error": "title required"},400
+		return add_task(title)
