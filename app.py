eb29d5421023a41fdfc80fbfbe7cb96a2963bb4b
IlhamF4 <52096634+IlhamF4@users.noreply.github.com>
2026-04-21 15:57:10 +0700

    Day 6: add DELETE /tasks/<int:task_id> endpoint

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from logic import get_tasks, add_task, update_task
+from logic import get_tasks, add_task, update_task, delete_task
 
 app = Flask(__name__)
 
@@ -15,18 +15,28 @@ def tasks_route():
 		title = request.args.get("title")
 		if not title:
 			return {"error": "title required"},400
-		return jsonify(add_task(title))
+		result = add_task(title)
+		return jsonify({"message": "added", "task": result})
 
 @app.route("/tasks/<int:task_id>",methods=["PUT"])
 def task_update(task_id):
 	title = request.args.get("title")
 	if not title:
 		return {"error": "title required"},400
+		
 	result = update_task(task_id,title)
 	if result is None:
 		return {"error": "id not found"},404
 	else:
-		return jsonify(result)
+		return jsonify({"message": "updated", "task": result})
 
+@app.route("/tasks/<int:task_id>",methods=["DELETE"])
+def task_delete(task_id):
+	result = delete_task(task_id)
+	if result is None:
+		return {"error": "id not found"},404
+	else:
+		return jsonify({"message": "deleted", "task": result})
+		
 if __name__ == "__main__":
 	app.run(debug=True)
--- logic.py
+++ logic.py
@@ -9,17 +9,25 @@ def add_task(title):
 	new_task = {"id": data.current_id, "title":title}
 	data.tasks.append(new_task)
 	data.current_id += 1
-	return {"message": "added", "task": new_task}
+	return new_task
 
 def update_task(task_id, title):
 	task_index = find_task_index(task_id)
 	if task_index is None:
 		return None
 	data.tasks[task_index]["title"] = title
-	return {"message": "updated", "task": data.tasks[task_index]}
+	return data.tasks[task_index]
 
 def find_task_index(task_id):
 	for i, task in enumerate(data.tasks):
 		if task["id"] == task_id:
 			return i
 	return None
+	
+def delete_task(task_id):
+	task_index = find_task_index(task_id)
+	if task_index is None:
+		return None
+	del_task = data.tasks.pop(task_index)
+	return del_task
+	
