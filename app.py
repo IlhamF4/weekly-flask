c69ef9ebc2c68b83c51ba5f1a9d980c951dc4d07
IlhamF4 <52096634+IlhamF4@users.noreply.github.com>
2026-04-27 14:38:23 +0700

    Day 12: add POST /users and GET /users

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, jsonify, request
-from werkzeug.exceptions import BadRequest, NotFound
-from logic import get_tasks, add_task, update_task, delete_task
+from werkzeug.exceptions import BadRequest, NotFound, HTTPException
+from logic import get_tasks, add_task, update_task, delete_task, init_db, check_user, add_user, get_users
 
 app = Flask(__name__)
 
+#Helper functions
 def parse_json():
 	if not request.is_json:
 		raise BadRequest("content must be json")
@@ -20,19 +21,23 @@ def parse_json():
 		raise BadRequest("request body cannot be empty")
 		
 	return data
-	
-	
-def validate_title(value):
-	if not isinstance(value,str):
-		raise BadRequest("title must be a string")
-	
-	title = value.strip()	
-	if title == "":
-		raise BadRequest("title cannot be empty")
-		
-	return title
 
 
+# Helper users
+def validate_user_id(value):
+	if not isinstance(value, int):
+		raise BadRequest("invalid type")
+	
+	if value <= 0:
+		raise BadRequest("user id must be a positive integer")
+	
+	if check_user(value) is None:
+		raise NotFound("user not found")
+	
+	return value
+
+
+# Helper tasks
 def parse_bool(value):
 	if value is None:
 		return None
@@ -55,6 +60,7 @@ def validate_int(value):
 		raise BadRequest("input must be a positive integer")
 		
 	return int(value)
+		
 		
 def parse_page(value):
 	if value is None:
@@ -94,6 +100,17 @@ def parse_search(value):
 		raise BadRequest("search cannot be empty")
 	
 	return value
+
+
+def validate_title(value):
+	if not isinstance(value,str):
+		raise BadRequest("title must be a string")
+	
+	title = value.strip()	
+	if title == "":
+		raise BadRequest("title cannot be empty")
+		
+	return title
 	
 	
 def validate_done(value):
@@ -102,7 +119,37 @@ def validate_done(value):
 		
 	return value
 	
+# Validation users
+def get_user_id():
+	if "X-User-Id" not in request.headers:
+		raise BadRequest("X-User-Id must exist in header")
+		
+	user_id = request.headers.get("X-User-Id")
 	
+	user_id = validate_user(user_id)
+	
+	return user_id
+
+
+def validate_username(value):
+	if value is None:
+		raise BadRequest("username is required")
+	
+	value = value.strip()
+	
+	if value == "":
+		raise BadRequest("username cannot be empty")
+	
+	return value
+
+def validate_add_user():
+	username = request.args.get("username")
+	
+	validated_username = validate_username(username)
+	
+	return {"username": validated_username}
+
+# Validation of tasks route
 def validate_create_task(data):
 	if "title" not in data:
 		raise BadRequest("title is required")
@@ -150,7 +197,55 @@ def validate_update_task(data):
 
 @app.route("/")
 def hello():
-	return jsonify({"message": "hello world"})
+	user_id = get_user_id()
+	return jsonify({"message": "hello world", "data": user_id})
+
+
+# Users area
+@app.route("/users", methods=["POST"])
+def add_user_route():
+	validated = validate_add_user()
+	
+	user = add_user(validated["username"])
+	
+	return jsonify({"data": user, "message": "user added"})
+
+
+@app.route("/users", methods=["GET"])
+def get_users_route():
+	users = get_users()
+	
+	return jsonify({
+		"data": users,
+		"count": len(users)
+	})
+
+
+@app.route("/users", methods=["PUT"])
+def update_user_route():
+	pass
+
+
+@app.route("/users", methods=["DELETE"])
+def delete_user_route():
+	pass
+
+
+#Tasks area
+@app.route("/tasks", methods=["POST"])
+def create_task_route():
+	data = parse_json()
+	
+	validated = validate_create_task(data)
+	
+	title = validated["title"]
+	
+	result = add_task(title)
+	
+	return jsonify({
+		"data": result,
+		"message": "Task created"
+		}),201
 
 
 @app.route("/tasks", methods=["GET"])
@@ -163,22 +258,16 @@ def get_tasks_route():
 	page = validated["page"]
 	limit = validated["limit"]
 	
-	tasks = get_tasks(done, search, sort, page, limit)
+	result = get_tasks(done, search, sort, page, limit)
 	
-	return jsonify({"tasks": tasks}),200
-
-
-@app.route("/tasks", methods=["POST"])
-def create_task_route():
-	data = parse_json()
-	
-	validated = validate_create_task(data)
-	
-	title = validated["title"]
-	
-	result = add_task(title)
-	
-	return jsonify({"message": "added", "task": result}),201
+	return jsonify({
+		"data": result,
+		"meta": {
+			"page": page,
+			"limit": limit,
+			"count": len(result)
+		}
+	}), 200
 
 
 @app.route("/tasks/<int:task_id>",methods=["PUT"])
@@ -195,7 +284,10 @@ def update_task_route(task_id):
 	if result is None:
 		raise NotFound("task not found")
 	
-	return jsonify({"message": "updated", "task": result}),200
+	return jsonify({
+		"data": result,
+		"message": "Task updated"
+		}), 200
 
 
 @app.route("/tasks/<int:task_id>", methods=["DELETE"])
@@ -205,8 +297,20 @@ def delete_task_route(task_id):
 	if result is None:
 		raise NotFound("task not found")
 	
-	return jsonify({"message": "deleted", "task": result}),200
+	return jsonify({
+		"data": result,
+		"message": "Task deleted"
+	}), 200
 	
+
+@app.errorhandler(HTTPException)
+def handle_http_exception(e):
+			return jsonify({
+				"error": e.description
+			}), e.code
 			
+
+init_db()
+
 if __name__ == "__main__":
 	app.run(debug=True)
--- logic.py
+++ logic.py
@@ -8,13 +8,21 @@ def set_row_factory(conn):
 	conn.row_factory = sqlite3.Row
 
 
-def row_to_list(rows):
-	return [row_to_dict(row) for row in rows]
+def tasks_row_list(rows):
+	return [tasks_row_dict(row) for row in rows]
 
 
-def row_to_dict(row):
+def tasks_row_dict(row):
 	return {"id": row["id"], "title": row["title"], "done": bool(row["done"])}
 	
+	
+def users_row_list(rows):
+	return [users_row_dict(row) for row in rows]
+
+
+def users_row_dict(row):
+	return {"user_id": row["user_id"], "username": row["username"]}
+
 
 def find_task(task_id):
 	conn = get_connection()
@@ -30,9 +38,21 @@ def find_task(task_id):
 	if task is None:
 		return None
 		
-	return row_to_dict(task)
+	return tasks_row_dict(task)
 	
+
+def check_user(user_id):
+	conn = get_connection()
+	cur = conn.cursor()
 	
+	cur.execute("SELECT id FROM users WHERE id = :id", {"id": user_id})
+	
+	result = cur.fetchone()
+	
+	conn.close()
+	
+	return result
+
 def init_db():
 	conn = get_connection()
 	cur = conn.cursor()
@@ -45,10 +65,46 @@ def init_db():
 	)
 	""")
 	
+	cur.execute("""
+		CREATE TABLE IF NOT EXISTS users(
+			user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+			username TEXT
+		)
+	""")
+	
 	conn.commit()
 	conn.close()
 
 
+# Users area
+def add_user(username):
+	conn = get_connection()
+	set_row_factory(conn)
+	cur = conn.cursor()
+	
+	cur.execute("INSERT INTO users (username) VALUES (:username)", {"username": username})
+	conn.commit()
+	
+	cur.execute("SELECT user_id, username FROM users WHERE user_id = :user_id", {"user_id": cur.lastrowid})
+	user = users_row_dict(cur.fetchone())
+	
+	return user
+
+
+def get_users():
+	conn = get_connection()
+	set_row_factory(conn)
+	cur = conn.cursor()
+	
+	cur.execute("SELECT user_id, username FROM users")
+	users = users_row_list(cur.fetchall())
+	
+	conn.close()
+	
+	return users
+	
+	
+# Tasks area
 def add_task(title):
 	conn = get_connection()
 	set_row_factory(conn)
@@ -58,13 +114,13 @@ def add_task(title):
 	conn.commit()
 	
 	cur.execute("SELECT id, title, done FROM tasks WHERE id = :id", {"id": cur.lastrowid})
-	task = row_to_dict(cur.fetchone())
+	task = tasks_row_dict(cur.fetchone())
 	conn.close()
 	
 	return task
 
 
-def get_tasks(done=None, sort=None, search=None, page=1, limit=10):
+def get_tasks(done=None, search=None, sort=None, page=1, limit=10):
 	conn = get_connection()
 	set_row_factory(conn)
 	cur = conn.cursor()
@@ -78,6 +134,7 @@ def get_tasks(done=None, sort=None, search=None, page=1, limit=10):
 	if done is not None:
 		conditions.append("done = :done")
 		params["done"] = int(done)
+	#return done
 	if search is not None:
 		conditions.append("title LIKE :search")
 		params["search"] = f"%{search}%"
@@ -86,14 +143,16 @@ def get_tasks(done=None, sort=None, search=None, page=1, limit=10):
 		query += " WHERE " + " AND ".join(conditions)
 		
 	if sort is not None:
+		#return sort
 		query += f" ORDER BY id {sort} "
+		#params["sort"] = f"{sort}"
 	
 	query += " LIMIT :limit OFFSET :offset"
 	params["limit"] = limit
 	params["offset"] = offset
 
 	cur.execute(query, params)
-	tasks = row_to_list(cur.fetchall())
+	tasks = tasks_row_list(cur.fetchall())
 	
 	conn.close()
 	
