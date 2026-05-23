b8cda083320488f88e875418556138af3fb9c2d5
IlhamF4 <52096634+IlhamF4@users.noreply.github.com>
2026-05-23 20:28:41 +0700

    Day 20: add limit and page, also added restore comment

--- logic/comments.py
+++ logic/comments.py
@@ -33,7 +33,7 @@ def add_comment(user_id, task_id, content):
 	return row_to_dict(comment)
 
 
-def get_comments(user_id, task_id, include_deleted=False):
+def get_comments(user_id, task_id, include_deleted=False, page=1, limit=10):
 	conn = get_connection(config.DB_NAME)
 	set_row_factory(conn)
 	cur = conn.cursor()
@@ -42,12 +42,20 @@ def get_comments(user_id, task_id, include_deleted=False):
 	if task in (NOT_FOUND, FORBIDDEN):
 		return task
 	
+	limit = max(1, min(limit, 100))
+	page = max(1, page)
+	offset = (page - 1) * limit
+	
 	query = "SELECT comment_id, task_id, user_id, content, created_at FROM comments WHERE task_id = :task_id"
 	params = {"task_id": task_id}
 	
 	if not include_deleted:
 		query += " AND deleted = :deleted"
 		params["deleted"] = False
+	
+	query += " LIMIT :limit OFFSET :offset"
+	params["limit"] = limit
+	params["offset"] = offset
 		
 	cur.execute(query, params )
 	comments = cur.fetchall()
@@ -76,6 +84,31 @@ def delete_comment(user_id, comment_id):
 	
 	#cur.execute("DELETE FROM comments WHERE comment_id = :comment_id", {"comment_id": comment_id})
 	cur.execute("UPDATE comments SET deleted = True WHERE comment_id = :comment_id", {"comment_id": comment_id})
+	conn.commit()
+	
+	cur.execute("SELECT comment_id, task_id, user_id, content, created_at, deleted FROM comments WHERE comment_id = :comment_id", {"comment_id": comment_id})
+	comment = cur.fetchone()
+	
+	conn.close()
+	
+	return row_to_dict(comment) | {"deleted": bool(comment["deleted"])}
+	
+	
+def restore_comment(user_id, comment_id):
+	conn = get_connection(config.DB_NAME)
+	set_row_factory(conn)
+	cur = conn.cursor()
+	
+	#we can merge into 1 helper function
+	cur.execute("SELECT user_id, deleted FROM comments WHERE comment_id = :comment_id", {"comment_id": comment_id})
+	comment = cur.fetchone()
+	
+	if comment is None: 
+		return NOT_FOUND
+	if user_id != int(comment["user_id"]):
+		return FORBIDDEN
+		
+	cur.execute("UPDATE comments SET deleted = False WHERE comment_id = :comment_id", {"comment_id": comment_id})
 	conn.commit()
 	
 	cur.execute("SELECT comment_id, task_id, user_id, content, created_at, deleted FROM comments WHERE comment_id = :comment_id", {"comment_id": comment_id})
